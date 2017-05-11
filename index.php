8432ac4c8afd5326777ca2972e4e49164ec8fd71
jorense <jerrymiah.orense@gmail.com>
2017-05-11 16:11:27 +0800

    added photos and modified css, homepage

--- index.php
+++ index.php
@@ -98,12 +98,45 @@ include "module.inc";
             </div>
         </div>
 
-    </div>
+</div>
 
     <!--body content starts here-->
 </div>
 <div class="full-wrapper">
-    <img src="images/background-full-home.jpg" class="img-responsive">
+<div id="homepage-carousel" class="carousel slide" data-ride="carousel" data-interval="100000">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#homepage-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#homepage-carousel" data-slide-to="1"></li>
+    <li data-target="#homepage-carousel" data-slide-to="2"></li>
+    <li data-target="#homepage-carousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/homepage-carousel-1.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="images/homepage-carousel-2.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="images/homepage-carousel-3.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#homepage-carousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#homepage-carousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </div>
 <div class="full-wrapper">
     <div id="form-container">
